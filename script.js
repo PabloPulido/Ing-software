ab80e8d5eab786fb3d920a05c001a88daf9bd5d6
PabloPulido <juan.pulido2227@gmail.com>
2022-09-02 15:01:58 -0500

    se hicieron los ultimos cambios al formulario

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-function enviar(){
 
-    location.href = "index2.html";
+const inputUsuario =document.querySelector(".usuario");
+const inputClave =document.querySelector(".clave");
+const btn =document.querySelector (".enviar");
+
+
+
+
+btn.addEventListener("click", envioSolicitud);
+
+const mensaje =document.querySelector(".mensaje")
+
+function envioSolicitud(){
+
+    const usuario= inputUsuario.value;
+    const clave = inputClave.value
+   
+
+
+    if(usuario!=="pruba2022" && clave !== "prueba2022" ){
+
+        
+        mensaje.innerText ="el usuario y contraseña es incorrecto"
+
+    }
+    
+    
+    else {
+    
+        
+        
+        window.location= "index2.html"
+
+
 
 }
+
+
+
+
+   
+
+    }
+    
+   
+   
+   
+    
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
